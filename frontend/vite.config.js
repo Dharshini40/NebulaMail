ca9a563635923d157da5156dc2f7c4938d6c9441
Dharshini Rathinam <dharshinirathinam40@gmail.com>
2026-09-08 11:08:02 +0530

    fix frintend host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    allowedHosts: ['nebulamail-frontend.onrender.com'],
     proxy: {
       '/api': {
         target: 'http://localhost:4000',
